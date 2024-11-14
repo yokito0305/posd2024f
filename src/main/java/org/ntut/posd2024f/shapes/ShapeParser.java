1de69a78ff13e8a821a571781afe5170d4432a0b
林晉揚 <113598087@cc.ntut.edu.tw>
2024-11-14 23:39:13 +0800

    初步完成 ShapeParser.java

--- src/main/java/org/ntut/posd2024f/shapes/ShapeParser.java
+++ src/main/java/org/ntut/posd2024f/shapes/ShapeParser.java
@@ -1,15 +1,221 @@
 package org.ntut.posd2024f.shapes;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ShapeParser {
-    List<Shape> shapes;
+    private List<Shape> shapes = null;
+    private Scanner scanner = null;
+    private ShapeBuilder builder = null;
 
     public ShapeParser(File file) {
+        try {
+            scanner = new Scanner(file);
+        } catch (Exception e) {
+            throw new RuntimeException("File not found");
+        }
+
+        shapes = new ArrayList<Shape>();
     }
 
     public void parse() {
+        builder = new ShapeBuilder();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().trim();
+            parseLine(line);
+        }
+
+        shapes = builder.getResult();
+    }
+
+    private void parseLine(String line) {
+        String[] info = line.split(", ");
+            switch (info[0].split(" ")[0]) {
+                case "Circle":
+                    parseCircle(info);
+                    break;
+                case "Rectangle":
+                    parseRectangle(info);
+                    break;
+                case "Triangle":
+                    parseTriangle(info);
+                    break;
+                case "ConvexPolygon":
+                    parseConvexPolygon(info);
+                    break;
+                case "CompoundShape":
+                    parseCompoundShape(info, getSpacesNum(line));
+                    break;
+                default:
+                    break;
+            }
+    }
+
+    private void parseCircle(String[] info) {
+        String color = null;
+        String text = null;
+        double radius = Double.parseDouble(info[0].split(" ")[1]);
+        for (int i = 1; i < info.length; i++) {
+            String[] keyValue = info[i].split("=");
+            switch (keyValue[0]) {
+                case "color":
+                    color = keyValue[1];
+                    break;
+                case "text":
+                    text = keyValue[1];
+                    break;
+                default:
+                    break;
+            }
+        }
+        builder.buildCircle(radius, color, text);
+    }
+
+    private void parseRectangle(String[] info) {
+        String color = null;
+        String text = null;
+        double width = Double.parseDouble(info[0].split(" ")[1]);
+        double height = Double.parseDouble(info[0].split(" ")[2]);
+        for (int i = 1; i < info.length; i++) {
+            String[] keyValue = info[i].split("=");
+            switch (keyValue[0]) {
+                case "color":
+                    color = keyValue[1];
+                    break;
+                case "text":
+                    text = keyValue[1];
+                    break;
+                default:
+                    break;
+            }
+        }
+        builder.buildRectangle(width, height, color, text);
+    }
+
+    private TwoDimensionalVector getTwoDimensionalVector(String vector) {
+        if (!vector.startsWith("[")) {
+            throw new IllegalArgumentException("Expected token '['");
+        }
+        if (!vector.endsWith("]")) {
+            throw new IllegalArgumentException("Expected token ']'");
+        }
+        if (!vector.contains(",")) {
+            throw new IllegalArgumentException("Expected token ','");
+        }
+        
+        String[] coordinates = vector.substring(1, vector.length() - 1).split(",");
+        return new TwoDimensionalVector(Integer.parseInt(coordinates[0].trim()), Integer.parseInt(coordinates[1].trim()));
+    }
+
+    private void parseTriangle(String[] info) {
+        String color = null;
+        String text = null;
+        List<TwoDimensionalVector> vectors = new ArrayList<TwoDimensionalVector>();
+        TwoDimensionalVector v1 = getTwoDimensionalVector(info[0].split(" ")[1]);
+        TwoDimensionalVector v2 = getTwoDimensionalVector(info[0].split(" ")[2]);
+        TwoDimensionalVector v3 = getTwoDimensionalVector(info[0].split(" ")[3]);
+        vectors.add(v1);
+        vectors.add(v2);
+        vectors.add(v3);
+        for (int i = 1; i < info.length; i++) {
+            String[] keyValue = info[i].split("=");
+            switch (keyValue[0]) {
+                case "color":
+                    color = keyValue[1];
+                    break;
+                case "text":
+                    text = keyValue[1];
+                    break;
+                default:
+                    break;
+            }
+        }
+        builder.buildTriangle(vectors, color, text);
+    }
+
+    private void parseConvexPolygon(String[] info) {
+        String color = null;
+        String text = null;
+        List<TwoDimensionalVector> vectors = new ArrayList<TwoDimensionalVector>();
+        String[] vectorStrings = info[0].split(" ");
+        for (int i = 1; i < vectorStrings.length; i++) {
+            vectors.add(getTwoDimensionalVector(vectorStrings[i]));
+        }
+
+        for (int i = 1; i < info.length; i++) {
+            String[] keyValue = info[i].split("=");
+            switch (keyValue[0]) {
+                case "color":
+                    color = keyValue[1];
+                    break;
+                case "text":
+                    text = keyValue[1];
+                    break;
+                default:
+                    break;
+            }
+        }
+        builder.buildConvexPolygon(vectors, color, text);
+    }
+
+    private int getSpacesNum(String line) {
+        int count = 0;
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == ' ') {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+
+    private void parseCompoundShape(String[] info, int spacesNum) {
+        String color = null;
+        String text = null;
+
+        int i = 1;
+        for (; i < info.length; i++) {
+            String[] keyValue = info[i].split("=");
+            switch (keyValue[0]) {
+                case "color":
+                    color = keyValue[1].split(" ")[0];
+                    break;
+                case "text":
+                    text = keyValue[1].substring(0, keyValue[1].length() - 2);
+                    break;
+                default:
+                    break;
+            }
+        }
+        i--;
+        if (!info[i].contains("{")) {
+            throw new IllegalArgumentException("Expected token '{'");
+        }
+
+        builder.beginBuildCompoundShape(color, text);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            int nextLineSpaces = getSpacesNum(line);
+            line = line.trim();
+            if (nextLineSpaces == spacesNum) {
+                if (line.equals("}")) {
+                    builder.endBuildCompoundShape();
+                    return;
+                }
+
+                if (!info[i].contains("}")) {
+                    throw new IllegalArgumentException("Invalid indentation");
+                }
+            }
+            
+            parseLine(line);
+        }
+
+        throw new IllegalArgumentException("Expected token '}'");
     }
 
     public List<Shape> getResult() {
--- src/test/java/org/ntut/posd2024f/shapes/ShapeParserTest.java
+++ src/test/java/org/ntut/posd2024f/shapes/ShapeParserTest.java
@@ -1,4 +1,328 @@
 package org.ntut.posd2024f.shapes;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
 public class ShapeParserTest {
+    // Test open file
+    @Test
+    public void testParserOpenFile() {
+        String path = "src/test_data/complex_shape.txt";
+        File file = new File(path);
+        new ShapeParser(file);
+    }
+
+    @Test
+    public void testParserOpenFileFail() {
+        String path = "src/test_data/notfount.txt";
+        File file = new File(path);
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            new ShapeParser(file);
+        });
+        assertEquals("File not found", exception.getMessage());
+    }
+
+    // Test Circle
+    @Test
+    public void testParserWithCircle() {
+        String path = "src/test_data/circle.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        parser.parse();
+
+        List<Shape> shapes = parser.getResult();
+        assertEquals(1, shapes.size());
+        assertEquals(TextedShape.class, shapes.get(0).getClass());
+        assertEquals("This is a circle", ((TextedShape)shapes.get(0)).getText());
+        Shape coloredShape = ((TextedShape)shapes.get(0)).getShape();
+        assertEquals(ColoredShape.class, coloredShape.getClass());
+        assertEquals("RED", ((ColoredShape)coloredShape).getColor());
+        Shape circle = ((ColoredShape)coloredShape).getShape();
+        assertEquals(Circle.class, circle.getClass());
+        assertEquals(3.0, ((Circle)circle).getRadius());
+    }
+
+    @Test
+    public void testParserWithRedCircle() {
+        String path = "src/test_data/circle_red.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        parser.parse();
+
+        List<Shape> shapes = parser.getResult();
+        assertEquals(1, shapes.size());
+        assertEquals(ColoredShape.class, shapes.get(0).getClass());
+        assertEquals("RED", ((ColoredShape)shapes.get(0)).getColor());
+        Shape circle = ((ColoredShape)shapes.get(0)).getShape();
+        assertEquals(Circle.class, circle.getClass());
+        assertEquals(3.0, ((Circle)circle).getRadius());
+    }
+
+    @Test
+    public void testParserWithCircleWithText() {
+        String path = "src/test_data/circle_text.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        parser.parse();
+
+        List<Shape> shapes = parser.getResult();
+        assertEquals(1, shapes.size());
+        assertEquals(TextedShape.class, shapes.get(0).getClass());
+        assertEquals("This is a circle", ((TextedShape)shapes.get(0)).getText());
+        Shape circle = ((TextedShape)shapes.get(0)).getShape();
+        assertEquals(Circle.class, circle.getClass());
+        assertEquals(3.0, ((Circle)circle).getRadius());
+    }
+
+    // Test Rectangle
+    @Test
+    public void testParserWithRectangle() {
+        String path = "src/test_data/rectangle.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        parser.parse();
+
+        List<Shape> shapes = parser.getResult();
+        assertEquals(1, shapes.size());
+        assertEquals(TextedShape.class, shapes.get(0).getClass());
+        assertEquals("This is a rectangle", ((TextedShape)shapes.get(0)).getText());
+        Shape coloredShape = ((TextedShape)shapes.get(0)).getShape();
+        assertEquals(ColoredShape.class, coloredShape.getClass());
+        assertEquals("RED", ((ColoredShape)coloredShape).getColor());
+        Shape rectangle = ((ColoredShape)coloredShape).getShape();
+        assertEquals(Rectangle.class, rectangle.getClass());
+        // area = 3.0 * 4.0 = 12.0, perimeter = 2 * (3.0 + 4.0) = 14.0
+        assertEquals(12.0, ((Rectangle)rectangle).area());
+        assertEquals(14.0, ((Rectangle)rectangle).perimeter());
+    }
+
+    @Test
+    public void testParserWithRedRectangle() {
+        String path = "src/test_data/rectangle_red.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        parser.parse();
+
+        List<Shape> shapes = parser.getResult();
+        assertEquals(1, shapes.size());
+        assertEquals(ColoredShape.class, shapes.get(0).getClass());
+        assertEquals("RED", ((ColoredShape)shapes.get(0)).getColor());
+        Shape rectangle = ((ColoredShape)shapes.get(0)).getShape();
+        assertEquals(Rectangle.class, rectangle.getClass());
+        // area = 3.0 * 4.0 = 12.0, perimeter = 2 * (3.0 + 4.0) = 14.0
+        assertEquals(12.0, ((Rectangle)rectangle).area());
+        assertEquals(14.0, ((Rectangle)rectangle).perimeter());
+    }
+
+    @Test
+    public void testParserWithRectangleWithText() {
+        String path = "src/test_data/rectangle_text.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        parser.parse();
+
+        List<Shape> shapes = parser.getResult();
+        assertEquals(1, shapes.size());
+        assertEquals(TextedShape.class, shapes.get(0).getClass());
+        assertEquals("This is a rectangle", ((TextedShape)shapes.get(0)).getText());
+        Shape rectangle = ((TextedShape)shapes.get(0)).getShape();
+        assertEquals(Rectangle.class, rectangle.getClass());
+        // area = 3.0 * 4.0 = 12.0, perimeter = 2 * (3.0 + 4.0) = 14.0
+        assertEquals(12.0, ((Rectangle)rectangle).area());
+        assertEquals(14.0, ((Rectangle)rectangle).perimeter());
+    }
+
+    // Test Triangle
+    @Test
+    public void testParserWithTriangle() {
+        String path = "src/test_data/triangle.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        parser.parse();
+
+        List<Shape> shapes = parser.getResult();
+        assertEquals(1, shapes.size());
+        assertEquals(TextedShape.class, shapes.get(0).getClass());
+        assertEquals("This is a triangle", ((TextedShape)shapes.get(0)).getText());
+        Shape coloredShape = ((TextedShape)shapes.get(0)).getShape();
+        assertEquals(ColoredShape.class, coloredShape.getClass());
+        assertEquals("RED", ((ColoredShape)coloredShape).getColor());
+        Shape triangle = ((ColoredShape)coloredShape).getShape();
+        assertEquals(Triangle.class, triangle.getClass());
+        // area = 6, perimeter = 12
+        assertEquals(6.0, ((Triangle)triangle).area());
+        assertEquals(12.0, ((Triangle)triangle).perimeter());
+    }
+
+    // Expected token '['
+    @Test
+    public void testParserTriangleWithIllegalLeftBracket() {
+        String path = "src/test_data/triangle_illegal.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            parser.parse();
+        });
+        parser.parse();
+        assertEquals("Expected token '['", exception.getMessage());
+    }
+
+    // Expected token ']'
+    @Test
+    public void testParserTriangleWithIllegalRightBracket() {
+        String path = "src/test_data/triangle_illegal2.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            parser.parse();
+        });
+        parser.parse();
+        assertEquals("Expected token ']'", exception.getMessage());
+    }
+
+    // Expected token ','
+    @Test
+    public void testParserTriangleWithIllegalComma() {
+        String path = "src/test_data/triangle_illegal3.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            parser.parse();
+        });
+        parser.parse();
+        assertEquals("Expected token ','", exception.getMessage());
+    }
+
+    // test convexPolygon
+    @Test
+    public void testParserWithConvexPolygon() {
+        String path = "src/test_data/convexPolygon.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        parser.parse();
+
+        List<Shape> shapes = parser.getResult();
+        assertEquals(1, shapes.size());
+        assertEquals(TextedShape.class, shapes.get(0).getClass());
+        assertEquals("This is a convexPolygon", ((TextedShape)shapes.get(0)).getText());
+        Shape coloredShape = ((TextedShape)shapes.get(0)).getShape();
+        assertEquals(ColoredShape.class, coloredShape.getClass());
+        assertEquals("RED", ((ColoredShape)coloredShape).getColor());
+        Shape convexPolygon = ((ColoredShape)coloredShape).getShape();
+        assertEquals(ConvexPolygon.class, convexPolygon.getClass());
+        // area = 6, perimeter = 12
+        assertEquals(6.0, ((ConvexPolygon)convexPolygon).area());
+        assertEquals(12.0, ((ConvexPolygon)convexPolygon).perimeter());
+     }
+
+    // Expected token '['
+    @Test
+    public void testParserConvexPolygonWithIllegalLeftBracket() {
+        String path = "src/test_data/convexPolygon_illegal.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            parser.parse();
+        });
+        parser.parse();
+        assertEquals("Expected token '['", exception.getMessage());
+    }
+
+    // Expected token ']'
+    @Test
+    public void testParserConvexPolygonWithIllegalRightBracket() {
+        String path = "src/test_data/convexPolygon_illegal2.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            parser.parse();
+        });
+        parser.parse();
+        assertEquals("Expected token ']'", exception.getMessage());
+    }
+
+    // Expected token ','
+    @Test
+    public void testParserConvexPolygonWithIllegalComma() {
+        String path = "src/test_data/convexPolygon_illegal3.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            parser.parse();
+        });
+        parser.parse();
+        assertEquals("Expected token ','", exception.getMessage());
+    }
+
+    // test compoundShape
+    @Test
+    public void testParserWithCompoundShape() {
+        String path = "src/test_data/compoundShape.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        parser.parse();
+
+        List<Shape> shapes = parser.getResult();
+        assertEquals(1, shapes.size());
+        assertEquals(CompoundShape.class, shapes.get(0).getClass());
+        Iterator<Shape> iterator = ((CompoundShape)shapes.get(0)).iterator();
+        assertEquals(Circle.class, iterator.next().getClass());
+        assertEquals(Rectangle.class, iterator.next().getClass());
+        assertFalse(iterator.hasNext());
+    }
+
+    // Expected token '{'
+    @Test
+    public void testParserCompoundShapeWithIllegalLeftBracket() {
+        String path = "src/test_data/compoundShape_illegal.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            parser.parse();
+        });
+        parser.parse();
+        assertEquals("Expected token '{'", exception.getMessage());
+    }
+
+    // Expected token '{'
+    @Test
+    public void testParserEmptyCompoundShapeWithIllegalLeftBracket() {
+        String path = "src/test_data/compoundShape_illegal3.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            parser.parse();
+        });
+        parser.parse();
+        assertEquals("Expected token '{'", exception.getMessage());
+    }
+
+    // Expected token '}'
+    @Test
+    public void testParserEmptyCompoundShapeWithIllegalRightBracket() {
+        String path = "src/test_data/compoundShape_illegal2.txt";
+        File file = new File(path);
+        ShapeParser parser = new ShapeParser(file);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            parser.parse();
+        });
+        parser.parse();
+        assertEquals("Expected token '}'", exception.getMessage());
+    }
+
+    // other test
+    @Test
+    public void testStringSplit() {
+        String line = "Circle 3.0";
+        // split : 'Circle 3.0', 'color=RED', 'text=This is a circle'
+        String[] info = line.split(", ");
+        assertEquals("Circle 3.0", info[0]);
+    }
 }
